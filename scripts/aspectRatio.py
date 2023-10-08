678aab41b49f0f889304205fd319e55308dc6c07
AlvaroPay <91126097+AlvaroPay@users.noreply.github.com>
2023-10-08 18:44:34 +0200

    Consider rotation for the refinement box in y

--- scripts/aspectRatio.py
+++ scripts/aspectRatio.py
@@ -1,7 +1,11 @@
+import numpy as np
+
 def calculateAR(bluff):
   x_ref_length = 1
   y_ref_length = int(bluff[1]+bluff[2])
-  if y_ref_length <= 5:
-      return 0.2*y_ref_length*x_ref_length
+  AoA = np.radians(int(bluff[3])*1.5)
+  
+  if y_ref_length <= 10:
+      return 0.1*y_ref_length*x_ref_length, (0.1*y_ref_length*x_ref_length)*np.cos(AoA)+np.sin(AoA)
   else:
-      return (y_ref_length/2 - 1.5)*x_ref_length
+      return (y_ref_length/5-1)*x_ref_length, ((y_ref_length/5-1)*x_ref_length)*np.cos(AoA)+np.sin(AoA)
